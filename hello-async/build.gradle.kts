6cdca1286c034f9553d09ff7e74edb5e00eb9c47
young-kim <ceo.youngkim@gmail.com>
2024-03-08 23:19:53 +0900

    feat: Add async module

---
+++ hello-async/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
