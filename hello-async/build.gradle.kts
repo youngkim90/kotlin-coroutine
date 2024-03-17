568a5d1b63a2d251fe365421b96e30bc9b56ab68
young-kim <ceo.youngkim@gmail.com>
2024-03-17 22:42:45 +0900

    feat: test asynchronized non blocking

--- hello-async/build.gradle.kts
+++ hello-async/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
   implementation("ch.qos.logback:logback-classic:1.4.11")
   implementation("io.github.microutils:kotlin-logging:3.0.5")
+  implementation("io.projectreactor:reactor-core:3.5.8")
   testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
