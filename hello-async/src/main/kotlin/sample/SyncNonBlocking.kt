26fb9170909135d1b95db94eb6cf66b2fdb28617
young-kim <ceo.youngkim@gmail.com>
2024-03-17 22:27:26 +0900

    feat: test synchronized non blocking

---
+++ hello-async/src/main/kotlin/sample/SyncNonBlocking.kt
@@ -0,0 +1,32 @@
+package org.example.sample
+
+import mu.KotlinLogging
+import java.util.concurrent.CompletableFuture
+import kotlin.system.measureTimeMillis
+
+private val logger = KotlinLogging.logger {}
+
+fun main() {
+  measureTimeMillis {
+    val taskA = subA()
+    subB()
+    while (!taskA.isDone) {
+      logger.debug { "... waiting A" }
+      Thread.sleep(100)
+    }
+  }.let { logger.debug { ">> time: $it ms" } }
+}
+
+private fun subA(): CompletableFuture<Unit> {
+  return CompletableFuture.supplyAsync {
+    logger.debug { "start A" }
+    Thread.sleep(2000)
+    logger.debug { "end A" }
+  }
+}
+
+private fun subB() {
+  logger.debug { "start B" }
+  Thread.sleep(1000)
+  logger.debug { "end B" }
+}
